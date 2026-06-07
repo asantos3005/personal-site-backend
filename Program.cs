872836613ff70a5fb422daf573b093ecb7b92211
Andrew Santos <andrewsan321@gmail.com>
2026-06-07 14:50:05 +1000

    Change frontend cors

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins("http://localhost:5173",
-            "https://personal-site-sigma-henna.vercel.app/")
+            "https://personal-site-sigma-henna.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
